45d293d1aae927db6e0a7e201f80386c0d06bf80
kappanjoe <6261485+kappanjoe@users.noreply.github.com>
2022-08-01 23:25:37 +0900

    Create Project 10 w/ challenges

---
+++ Project10/Project10/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Joseph Van Alstyne on 8/1/22.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
