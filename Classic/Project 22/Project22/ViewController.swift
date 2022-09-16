4234fefc9602aac5509f6644a876f16eabf894ed
kappanjoe <6261485+kappanjoe@users.noreply.github.com>
2022-09-16 15:01:13 +0900

    Complete Project 22 challenge 2

--- Classic/Project 22/Project22/ViewController.swift
+++ Classic/Project 22/Project22/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     var locationManager: CLLocationManager?
     var beaconDetectedAlertShown: Bool?
+    let uuidArr = ["EA040D34-E4D0-4C5E-A0D7-E35CEAF63A66", "E2C56DB5-DFFB-48D2-B060-D0F5A71096E0", "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5", "74278BDA-B644-4520-8F0C-720EAF059935"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,6 +43,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
         if let beacon = beacons.first {
+            stopScanning(exceptUUID: beacon.uuid.uuidString)
             update(distance: beacon.proximity)
             uuidReading.text = beacon.uuid.uuidString
             if !beaconDetectedAlertShown! {
@@ -56,13 +58,26 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     func startScanning() {
-        let uuid = UUID(uuidString: "EA040D34-E4D0-4C5E-A0D7-E35CEAF63A66")!
-        // Create constraint and add to dictionary (per current iBeacon documentation)
-        let constraint = CLBeaconIdentityConstraint(uuid: uuid)
-        let beaconRegion = CLBeaconRegion(beaconIdentityConstraint: constraint, identifier: uuid.uuidString)
-        
-        locationManager?.startMonitoring(for: beaconRegion)
-        locationManager?.startRangingBeacons(in: beaconRegion)
+        for uuid in uuidArr {
+            // Create constraint and add to dictionary (per current iBeacon documentation)
+            let constraint = CLBeaconIdentityConstraint(uuid: UUID(uuidString: uuid)!)
+            let beaconRegion = CLBeaconRegion(beaconIdentityConstraint: constraint, identifier: uuid)
+            
+            locationManager?.startMonitoring(for: beaconRegion)
+            locationManager?.startRangingBeacons(in: beaconRegion)
+        }
+    }
+    
+    func stopScanning(exceptUUID: String?) {
+        for uuid in uuidArr.filter({ element in
+            element != exceptUUID
+        }) {
+            let constraint = CLBeaconIdentityConstraint(uuid: UUID(uuidString: uuid)!)
+            let beaconRegion = CLBeaconRegion(beaconIdentityConstraint: constraint, identifier: uuid)
+            
+            locationManager?.stopMonitoring(for: beaconRegion)
+            locationManager?.stopRangingBeacons(in: beaconRegion)
+        }
     }
     
     func update(distance: CLProximity) {
